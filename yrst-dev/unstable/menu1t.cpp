7e11fdb0f6d76513710adfbd7b88d42e2a1ec8af
yrst <urryst@gmail.com>
2019-06-25 21:00:34 +0700

    Fix bug with excess cells.

--- yrst-dev/unstable/menu1t.cpp
+++ yrst-dev/unstable/menu1t.cpp
@@ -328,11 +328,14 @@ void(draw)(RenderWindow& window, char* team[], Font font, int size)
 
     //отрисовка 3го столбца--------------------------------
     if (stop == 1) {
-        if ((size % 4 < 4) && (size != 8) && (size != 12) && (size != 16)
-            && (size != 20) && (size != 24) && (size != 28) && (size != 32))
+        if ((size % 4 < 4) && (size != 4) && (size != 8) && (size != 12)
+            && (size != 16) && (size != 20) && (size != 24) && (size != 28)
+            && (size != 32))
             newsize = size / 4 + 1;
-        else
+        else if (size > 4)
             newsize = size / 4;
+        else
+            newsize = size / 4 - 1;
         c = 0;
         t = 97;
         for (int i = 0; i < (newsize); i++) {
@@ -357,10 +360,13 @@ void(draw)(RenderWindow& window, char* team[], Font font, int size)
     }
     //отрисовка 4го столбца--------------------------------
     if (stop == 2) {
-        if ((size % 8 < 8) && (size != 16) && (size != 24) && (size != 32))
+        if ((size % 8 < 8) && (size != 8) && (size != 16) && (size != 24)
+            && (size != 32))
             newsize = size / 8 + 1;
-        else
+        else if (size > 8)
             newsize = size / 8;
+        else
+            newsize = size / 8 - 1;
         c = 0;
         t = 205;
         for (int i = 0; i < (newsize); i++) {
@@ -387,7 +393,7 @@ void(draw)(RenderWindow& window, char* team[], Font font, int size)
     int i = 1;
     if ((stop == 3) && (i <= (size / 16))) {
         t = 423;
-        for (int i = 0; i < 2; i++) {
+        for (int i = 0; i < size / 16; i++) {
             for (int j = 1; j < 1000000; j++) {
                 j = j;
             }
