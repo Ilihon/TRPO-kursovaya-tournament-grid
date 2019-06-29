aca6a9b72f7ed44c111a6f29a199f8828e27c418
Ilihon <Ilia.Burdukovskiy@gmail.com>
2019-06-30 01:56:01 +0700

    Create test and thirdparty dirs.

---
+++ test/test.cpp
@@ -0,0 +1,19 @@
+#include "../src/column.h"
+#include "../src/draw.h"
+#include "../src/menu.h"
+#include "../src/steam.h"
+#include "gtest/gtest.h"
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <sstream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
